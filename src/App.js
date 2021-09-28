e79d22d6dc2358dffe3715b115cc27b33edb1f91
Nikhil <nikhilthakur24to7@gmail.com>
2021-09-28 10:29:33 +0530

    textApp

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '4eb757e4-e29d-43bc-9c6a-32c50131d88b';
+
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+
+        />
+    );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
